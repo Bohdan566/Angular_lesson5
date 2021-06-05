513864fe428a9cea936b40f8064b9ef0575c6afe
Bohdan566 <bohdan55566@gmail.com>
2021-06-05 23:57:04 +0300

    lesson_5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lesson5';
+
+  users : any[] = [];
+  user : any = {};
+
+  name = new FormControl('', [Validators.required, Validators.minLength(10), this.fuckValidator]);
+  age = new FormControl('', Validators.required);
+
+  myForm = new FormGroup({
+    name : this.name,
+    age : this.age
+  });
+
+  action(): any {
+    console.log(this.myForm);
+    this.user = {name: this.name.value, age: this.age.value};
+    this.users.push(this.user);
+
+    console.log(this.users)
+  }
+
+  fuckValidator(inputData: AbstractControl): any{
+    if (inputData.value.includes('fuck')){
+      return {fuck: true, msg : "Fuck is present here"}
+    }
+    return null;
+  }
 }
